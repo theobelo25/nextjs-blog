27188210f6ecd0b50f22674981ce1395ff46377d
Theo Belo <theo.belo25@gmail.com>
2023-06-27 15:54:47 -0400

    fixed env variable name

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -23,7 +23,7 @@ async function handler(req, res) {
     };
 
     let client;
-    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${mongodb_clustername}.kfc6amq.mongodb.net/${mongodb_database}?retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.kfc6amq.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
     try {
       client = await MongoClient.connect(connectionString);
